cbb1d6812b5d4922e53c59990997044c1b776a85
fett190158 <leinad.13.3@gmx.at>
2025-03-12 17:09:05 +0100

    Patient Class

--- src/arztpraxis/persons/Patient.java
+++ src/arztpraxis/persons/Patient.java
@@ -7,14 +7,24 @@ public class Patient {
     private String name;
     private String birthdate;
     private String address;
-    private String svnNummer;
+    private String svnNumber;
     
-    public Patient(int id, String name, String birthdate, String address, String svnNummer){
+    public Patient(int id, String name, String birthdate, String address, String svnNumber){
         this.id = id;
         this.name = name;
         this.birthdate = birthdate;
         this.address = address;
-        this.svnNummer = svnNummer;
+        this.svnNumber = svnNumber;
+    }
+    
+    public String getSVNNumber(){
+        return this.svnNumber;
+    }
+    public String lieferDaten(){
+        return this.id+".Patient "+this.name+"\n"
+               +"Geb. Datum: "+this.birthdate+"\n"
+               +"Adresse: "+this.address+"\n"
+               +"VersicherungsNr: "+this.svnNumber;
     }
     
 }
