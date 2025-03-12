bdb04ca728f85033cf4f79be0e762efe10222741
fett190158 <leinad.13.3@gmx.at>
2025-03-12 13:29:47 +0100

    Commit 1 Constructor, Attributes

--- src/arztpraxis/persons/Patient.java
+++ src/arztpraxis/persons/Patient.java
@@ -3,5 +3,18 @@ package arztpraxis.persons;
 
 
 public class Patient {
+    private int id;
+    private String name;
+    private String birthdate;
+    private String address;
+    private String svnNummer;
+    
+    public Patient(int id, String name, String birthdate, String address, String svnNummer){
+        this.id = id;
+        this.name = name;
+        this.birthdate = birthdate;
+        this.address = address;
+        this.svnNummer = svnNummer;
+    }
     
 }
